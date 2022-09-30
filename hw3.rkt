a3de0f4683b8d9b0cfe8c380bce987f7d2882fe0
shoerl <hoerl.s@northeastern.edu>
2022-09-30 15:43:12 -0400

    Do work on hw3

--- hw3.rkt
+++ hw3.rkt
@@ -8,6 +8,7 @@
              | { - <MUWAE> <MUWAE> }
              | { * <MUWAE> <MUWAE> }
              | { / <MUWAE> <MUWAE> }
+             | { sqrt <MUWAE> }
              | { with { <id> <MUWAE> } <MUWAE> }
              | <id>
 |#
@@ -19,6 +20,7 @@
   [Sub  MUWAE MUWAE]
   [Mul  MUWAE MUWAE]
   [Div  MUWAE MUWAE]
+  [Sqrt MUWAE]
   [Id   Symbol]
   [With Symbol MUWAE MUWAE])
 
@@ -37,6 +39,7 @@
     [(list '- lhs rhs) (Sub (parse-sexpr lhs) (parse-sexpr rhs))]
     [(list '* lhs rhs) (Mul (parse-sexpr lhs) (parse-sexpr rhs))]
     [(list '/ lhs rhs) (Div (parse-sexpr lhs) (parse-sexpr rhs))]
+    [(list 'sqrt lhs) (Sqrt (parse-sexpr lhs))]
     [else (error 'parse-sexpr "bad syntax in ~s" sexpr)]))
 
 (: parse : String -> MUWAE)
@@ -52,6 +55,7 @@
       {- E1 E2}[v/x]        = {- E1[v/x] E2[v/x]}
       {* E1 E2}[v/x]        = {* E1[v/x] E2[v/x]}
       {/ E1 E2}[v/x]        = {/ E1[v/x] E2[v/x]}
+      {sqrt E1}             = {sqrt E1[v/x]}
       y[v/x]                = y
       x[v/x]                = v
       {with {y E1} E2}[v/x] = {with {y E1[v/x]} E2[v/x]}
@@ -69,6 +73,7 @@
     [(Sub l r) (Sub (subst l from to) (subst r from to))]
     [(Mul l r) (Mul (subst l from to) (subst r from to))]
     [(Div l r) (Div (subst l from to) (subst r from to))]
+    [(Sqrt l) (Sqrt (subst l from to))]
     [(Id name) (if (eq? name from) to expr)]
     [(With bound-id named-expr bound-body)
      (With bound-id
@@ -77,46 +82,91 @@
              bound-body
              (subst bound-body from to)))]))
 
+
+(: sqrt+ : (Listof Number) -> (Listof Number))
+;; a version of `sqrt' that takes a list of numbers, and return a
+;; list with twice the elements, holding the two roots of each of
+;; the inputs; throws an error if any input is negative.
+(define (sqrt+ ns)
+  (cond [(null? ns) null]
+        [(< (first ns) 0) (error 'eval "requires a non-negative input")]
+        [else (let ([slv (sqrt (first ns))]) (cons slv (cons (- 0 slv) (sqrt+ (rest ns)))))]))
+
+
+
+(: bin-op :
+   (Number Number -> Number) (Listof Number) (Listof Number)
+   -> (Listof Number))
+;; applies a binary numeric function on all combinations
+;; of numbers from the two input lists, and return the
+;; list of all of the results
+(define (bin-op op ls rs)
+  (: helper : Number (Listof Number) -> (Listof Number))
+  (define (helper l rs)
+    (: f : Number -> Number)
+    (define (f r) (op l r))
+    (map f rs))
+  (if (null? ls)
+    null
+    (append (helper (first ls) rs) (bin-op op (rest ls) rs))))
+
+
 #| Formal specs for `eval':
      eval(N)         = N
      eval({+ E1 E2}) = eval(E1) + eval(E2)
      eval({- E1 E2}) = eval(E1) - eval(E2)
      eval({* E1 E2}) = eval(E1) * eval(E2)
      eval({/ E1 E2}) = eval(E1) / eval(E2)
+     eval({sqrt E1}) = eval(E1) ^ (1/2)
      eval(id)        = error!
      eval({with {x E1} E2}) = eval(E2[eval(E1)/x])
 |#
 
-(: eval : MUWAE -> Number)
+(: eval : MUWAE -> (Listof Number))
 ;; evaluates MUWAE expressions by reducing them to numbers
 (define (eval expr)
   (cases expr
-    [(Num n) n]
-    [(Add l r) (+ (eval l) (eval r))]
-    [(Sub l r) (- (eval l) (eval r))]
-    [(Mul l r) (* (eval l) (eval r))]
-    [(Div l r) (/ (eval l) (eval r))]
+    [(Num n) (list n)]
+    [(Add l r) (bin-op + (eval l) (eval r))]
+    [(Sub l r) (bin-op - (eval l) (eval r))]
+    [(Mul l r) (bin-op * (eval l) (eval r))]
+    [(Div l r) (bin-op / (eval l) (eval r))]
+    [(Sqrt e) (sqrt+ (eval e))]
     [(With bound-id named-expr bound-body)
      (eval (subst bound-body
                   bound-id
-                  (Num (eval named-expr))))]
+                  (Num (first (eval named-expr)))))]
     [(Id name) (error 'eval "free identifier: ~s" name)]))
 
-(: run : String -> Number)
+(: run : String -> (Listof Number))
 ;; evaluate a MUWAE program contained in a string
 (define (run str)
   (eval (parse str)))
 
 ;; tests
-(test (run "5") => 5)
-(test (run "{+ 5 5}") => 10)
-(test (run "{with {x 5} {+ x x}}") => 10)
-(test (run "{with {x {+ 5 5}} {+ x x}}") => 20)
-(test (run "{with {x 5} {with {y {- x 3}} {+ y y}}}") => 4)
-(test (run "{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}}") => 14)
-(test (run "{with {x 5} {+ x {with {x 3} 10}}}") => 15)
-(test (run "{with {x 5} {+ x {with {x 3} x}}}") => 8)
-(test (run "{with {x 5} {+ x {with {y 3} x}}}") => 10)
-(test (run "{with {x 5} {with {y x} y}}") => 5)
-(test (run "{with {x 5} {with {x x} x}}") => 5)
-(test (run "{with {x 1} y}") =error> "free identifier")
+
+;(test (run "5") => 5)
+;(test (run "{+ 5 5}") => 10)
+;(test (run "{with {x 5} {+ x x}}") => 10)
+;(test (run "{with {x {+ 5 5}} {+ x x}}") => 20)
+;(test (run "{with {x 5} {with {y {- x 3}} {+ y y}}}") => 4)
+;(test (run "{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}}") => 14)
+;(test (run "{with {x 5} {+ x {with {x 3} 10}}}") => 15)
+;(test (run "{with {x 5} {+ x {with {x 3} x}}}") => 8)
+;(test (run "{with {x 5} {+ x {with {y 3} x}}}") => 10)
+;(test (run "{with {x 5} {with {y x} y}}") => 5)
+;(test (run "{with {x 5} {with {x x} x}}") => 5)
+;(test (run "{with {x 1} y}") =error> "free identifier")
+      
+;(test (run "{sqrt 9}") => '(3 -3))
+;(test (run "{sqrt 1}") => '(1 -1))
+;(test (run "{sqrt 0}") => '(0 0))
+;(test (run "{sqrt -1}")
+;      =error> "requires a non-negative input")
+
+(test (run "{+ {sqrt 1} 3}")
+      => '(4 2))
+(test (run "{+ {/ {+ {sqrt 1} 3} 2} {sqrt 100}}")
+      => '(12 -8 11 -9))
+(test (run "{sqrt {+ 16 {* {+ 1 {sqrt 1}} {/ 9 2}}}}")
+      => '(5 -5 4 -4))
