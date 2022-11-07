6c7a59010e7d32c7c326c653e5f9335393adffa0
Brian Hill <hill.bri@northeastern.edu>
2022-11-06 23:00:38 -0500

    by-reference funcs

--- hw12.rkt
+++ hw12.rkt
@@ -10,6 +10,7 @@
            | <id>
            | { bind {{ <id> <TOY> } ... } <TOY> }
            | { fun { <id> ... } <TOY> }
+           | { rfun { <id> ... } <TOY> }
            | { if <TOY> <TOY> <TOY> }
            | { <TOY> <TOY> ... }
 |#
@@ -20,6 +21,7 @@
   [Id   Symbol]
   [Bind (Listof Symbol) (Listof TOY) TOY]
   [Fun  (Listof Symbol) TOY]
+  [RFun  (Listof Symbol) TOY]
   [Call TOY (Listof TOY)]
   [If   TOY TOY TOY])
 
@@ -40,17 +42,22 @@
      (match sexpr
        [(list 'bind (list (list (symbol: names) (sexpr: nameds))
                           ...)
-          body)
+              body)
         (if (unique-list? names)
-          (Bind names (map parse-sexpr nameds) (parse-sexpr body))
-          (error 'parse-sexpr "duplicate `bind' names: ~s" names))]
+            (Bind names (map parse-sexpr nameds) (parse-sexpr body))
+            (error 'parse-sexpr "duplicate `bind' names: ~s" names))]
        [else (error 'parse-sexpr "bad `bind' syntax in ~s" sexpr)])]
-    [(cons 'fun more)
+    [(or (cons 'fun more)
+         (cons 'rfun more))
      (match sexpr
        [(list 'fun (list (symbol: names) ...) body)
         (if (unique-list? names)
-          (Fun names (parse-sexpr body))
-          (error 'parse-sexpr "duplicate `fun' names: ~s" names))]
+            (Fun names (parse-sexpr body))
+            (error 'parse-sexpr "duplicate `fun' names: ~s" names))]
+       [(list 'rfun (list (symbol: names) ...) body)
+        (if (unique-list? names)
+            (RFun names (parse-sexpr body))
+            (error 'parse-sexpr "duplicate `rfun' names: ~s" names))]
        [else (error 'parse-sexpr "bad `fun' syntax in ~s" sexpr)])]
     [(cons 'if more)
      (match sexpr
@@ -81,26 +88,23 @@
 
 (define-type VAL
   [RktV  Any]
-  [FunV  (Listof Symbol) TOY ENV]
+  [FunV  (Listof Symbol) TOY ENV Boolean]
   [PrimV ((Listof VAL) -> VAL)])
 
-;; where does this go???
-(inst box VAL)
-
-(: raw-extend : (Listof Symbol) (Listof VAL) ENV -> ENV)
+(: raw-extend : (Listof Symbol) (Listof (Boxof VAL)) ENV -> ENV)
 ;; extends an environment with a new frame.
 (define (raw-extend names val-boxes env)
   (if (= (length names) (length val-boxes))
-    (FrameEnv (map (lambda ([name : Symbol] [val : (Boxof VAL)])
-                     (list name val))
-                   names val-boxes)
-              env)
-    (error 'raw-extend "arity mismatch for names: ~s" names)))
+      (FrameEnv (map (lambda ([name : Symbol] [val : (Boxof VAL)])
+                       (list name val))
+                     names val-boxes)
+                env)
+      (error 'raw-extend "arity mismatch for names: ~s" names)))
 
 (: extend : (Listof Symbol) (Listof VAL) ENV -> ENV)
 ;; extends an environment with a new frame.
 (define (extend names values env)
-  (raw-extend names (map (lambda (val) (box val)) values) env))
+  (raw-extend names (map (inst box VAL) values) env))
 
 (: lookup : Symbol ENV -> (Boxof VAL))
 ;; lookup a symbol in an environment, frame by frame,
@@ -111,8 +115,8 @@
     [(FrameEnv frame rest)
      (let ([cell (assq name frame)])
        (if cell
-         (second cell)
-         (lookup name rest)))]))
+           (second cell)
+           (lookup name rest)))]))
 
 (: unwrap-rktv : VAL -> Any)
 ;; helper for `racket-func->prim-val': unwrap a RktV wrapper in
@@ -131,7 +135,7 @@
 (define (racket-func->prim-val racket-func)
   (define list-func (make-untyped-list-function racket-func))
   (box (PrimV (lambda (args)
-           (RktV (list-func (map unwrap-rktv args)))))))
+                (RktV (list-func (map unwrap-rktv args)))))))
 
 ;; The global environment has a few primitives:
 (: global-environment : ENV)
@@ -144,8 +148,8 @@
                   (list '> (racket-func->prim-val >))
                   (list '= (racket-func->prim-val =))
                   ;; values
-                  (list 'true  (RktV #t))
-                  (list 'false (RktV #f)))
+                  (list 'true  (box (RktV #t)))
+                  (list 'false (box (RktV #f))))
             (EmptyEnv)))
 
 ;;; ----------------------------------------------------------------
@@ -163,22 +167,29 @@
     [(Bind names exprs bound-body)
      (eval bound-body (extend names (map eval* exprs) env))]
     [(Fun names bound-body)
-     (FunV names bound-body env)]
+     (FunV names bound-body env #f)]
+    [(RFun names bound-body)
+     (FunV names bound-body env #t)]
     [(Call fun-expr arg-exprs)
      (let ([fval (eval* fun-expr)]
            [arg-vals (map eval* arg-exprs)])
        (cases fval
          [(PrimV proc) (proc arg-vals)]
-         [(FunV names body fun-env)
-          (eval body (extend names arg-vals fun-env))]
+         [(FunV names body fun-env by-ref?)
+          (if by-ref?
+              ;(eval-body body (raw-extend names
+              (eval body (raw-extend names
+                                     (get-boxes arg-exprs env)
+                                     fun-env))
+              (eval body (extend names arg-vals fun-env)))]
          [else (error 'eval "function call with a non-function: ~s"
                       fval)]))]
     [(If cond-expr then-expr else-expr)
      (eval* (if (cases (eval* cond-expr)
                   [(RktV v) v] ; Racket value => use as boolean
                   [else #t])   ; other values are always true
-              then-expr
-              else-expr))]))
+                then-expr
+                else-expr))]))
 
 (: run : String -> Any)
 ;; evaluate a TOY program contained in a string
@@ -189,11 +200,27 @@
       [else (error 'run "evaluation returned a bad value: ~s"
                    result)])))
 
+(: get-boxes : (Listof TOY) ENV -> (Listof (Boxof VAL)))
+;; I think I implemented this wrong tbh, but can't test it yet
+(define (get-boxes arg-exprs env)
+  (: eval* : TOY -> VAL)
+  (define (eval* expr) (eval expr env))
+  (map (lambda ([val : VAL])
+         ;; assuming this will check for identifier, but maybe not
+         (if (symbol? val)
+             ((inst box VAL) val)
+             (error 'rfun "~s is a non-identifier" val)))
+       (map eval* arg-exprs)))
+
 ;;; ----------------------------------------------------------------
 ;;; Tests
 
 (test (run "{{fun {x} {+ x 1}} 4}")
       => 5)
+
+(test (run "{{rfun {x} {+ x 1}} 4}")
+      => 5)
+
 (test (run "{bind {{add3 {fun {x} {+ x 3}}}} {add3 1}}")
       => 4)
 (test (run "{bind {{add3 {fun {x} {+ x 3}}}
