5a3bf429097f0745227ca5b2021e4143291b86dc
shoerl <hoerl.s@northeastern.edu>
2022-12-07 14:06:02 -0500

    Finish up

--- hw17.rkt
+++ hw17.rkt
@@ -6,8 +6,7 @@
 (define-syntax automaton
   (syntax-rules (: ->)
     [(automaton init-state end-state
-       [state : (input-sym -> new-state) ...]
-       ...)
+                [state : (input-sym -> new-state) ...] ...)
      (lambda (string)
        (: state : (Listof Token) -> Boolean)
        ...
@@ -25,25 +24,25 @@
 (define cXr (automaton init end ; is the accepting state
                        [init : (c -> more)]
                        [more : (a -> more)
-                             (d -> more)
-                             (r -> end)]
+                               (d -> more)
+                               (r -> end)]
                        [end  : (r -> end)]))
 
 ;; tests:
 (test (cXr "cadr"))
 (test (cXr "cadadadadadadddddaaarrr"))
 (test (not (cXr "ccadr")))
-(test (not (cXr "c")))
+(test (not (cXr "c"))) ; A BAD TEST!
 
 (: div5 : String -> Boolean)
 ;; Determine whether a binary number is divisible by 5
 (define div5
   (automaton mod0 mod0
-    [mod0 : (0 -> mod0) (1 -> mod1)]
-    [mod1 : (0 -> mod2) (1 -> mod3)]
-    [mod2 : (0 -> mod4) (1 -> mod0)]
-    [mod3 : (0 -> mod1) (1 -> mod2)]
-    [mod4 : (0 -> mod3) (1 -> mod4)]))
+             [mod0 : (0 -> mod0) (1 -> mod1)]
+             [mod1 : (0 -> mod2) (1 -> mod3)]
+             [mod2 : (0 -> mod4) (1 -> mod0)]
+             [mod3 : (0 -> mod1) (1 -> mod2)]
+             [mod4 : (0 -> mod3) (1 -> mod4)]))
 
 (test (div5 ""))
 (test (div5 "0"))
@@ -53,33 +52,34 @@
 
 (define-syntax pushdown
   (syntax-rules (: ->)
-                [(pushdown init-state end-state
-                           [state: ((input-stream ...)
-                                    (input-stack ...)
-                                    ->
-                                    new-state
-                                    (new-stack ...)) ...] ...)
-                 (lambda (string)
-                   (: state : (Listof Token) (Listof Token) -> Boolean)
-                   (define (state stream stack)
-                     (match (list stream stack)
-                       [(list '() '()) (if (symbol=? 'state 'end-state) #t #f)]
-                       [(list (list-rest 'input-stream ... more-input)
-                              (list-rest 'input-stack  ... more-stack))
-                        (new-state more-input (list* 'new-stack ... more-stack))]
-                       ...
-                       [_ #f]))
-                   ...
-                   (init-state (append (explode-string string) '(*)) '(*)))]))
-                   
-                
+    [(pushdown init-state end-state
+               [state : ((input-stream ...) (input-stack ...)
+                          ->
+                          new-state (new-stack ...)) ...] ...)
+     (lambda (string)
+       (: state : (Listof Token) (Listof Token) -> Boolean)
+        ...
+       (define (state stream stack)
+         (match (list stream stack)
+           [(list '() '()) (if (symbol=? 'state 'end-state) #t #f)]
+           [(list (list-rest 'input-stream ... more-input)
+                  (list-rest 'input-stack ... more-stack))
+            (new-state more-input (list* 'new-stack ... more-stack))]
+           ...
+           [_ #f]))
+       ...
+       (init-state (append (explode-string string) '(*)) '(*)))]))
+            
+                              
 
 (: balanced : String -> Boolean)
 ;; Identifies strings that contain only balanced parentheses
-(define balanced (pushdown init init
-                   [init : ((open)  ()     -> init (open))
-                           ((close) (open) -> init ())
-                           ((*)     (*)    -> init ())]))
+(define balanced
+  (pushdown init init
+            [init : ((open) () -> init (open))
+                    ((close) (open) -> init ())
+                    ((*) (*)        -> init ())]))
+
 ;; tests:
 (test (balanced ""))
 (test (balanced "()"))
@@ -88,3 +88,23 @@
 (test (not (balanced "(")))
 (test (not (balanced ")")))
 (test (not (balanced ")(")))
+
+(: zeros=ones : String -> Boolean)
+;; Identifies strings of n 0s followed by n 1s
+(define zeros=ones
+  (pushdown 0s end
+    [0s  : ((0) ()  -> 0s  (A))
+           (()  ()  -> 1s  ())]
+    [1s  : ((1) (A) -> 1s  ())
+           ((*) (*) -> end (*))]
+    [end : (()  (*) -> end ())]))
+
+;; tests:
+(test (zeros=ones ""))
+(test (zeros=ones "01"))
+(test (zeros=ones "000111"))
+(test (not (zeros=ones "0")))
+(test (not (zeros=ones "11")))
+(test (not (zeros=ones "10")))
+(test (not (zeros=ones "00011")))
+(test (not (zeros=ones "00101111")))
